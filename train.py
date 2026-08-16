958aa51923dbeccc905d3b91191e2714a1b2bac6
userTGQ <100055084+userTGQ@users.noreply.github.com>
2026-08-16 23:24:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ parser.add_argument("--print_every", type=int, default=50, help="")
 parser.add_argument(
     "--save",
     type=str,
-    default="./logs/" + str(time.strftime("%Y-%m-%d-%H")) + "-",
+    default="./logs/" + str(time.strftime("%Y-%m-%d-%H-%S")) + "-",
     help="save path",
 )
 parser.add_argument(
